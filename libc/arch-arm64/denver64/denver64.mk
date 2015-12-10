376e3ff4eb10712392b6fa3756803a9d30457ae1
André Pinela <sheffzor@gmail.com>
2015-12-10 16:27:58 +0000

    libc: ARM64: denver64: optimized memmove breaks denver
    The optimized memmove on commit 95cc2b990baffb4f1149c16647d4e2e2069054e5
    breaks the boot on denver64/flounder, so we should use the old
    memmove to keep the generic memmove optimized.
    
    Change-Id: Ie5e4b5b56d0aeb4b4c22eb6450ac702c05b2ece2
    Signed-off-by: André Pinela <sheffzor@gmail.com>

--- libc/arch-arm64/denver64/denver64.mk
+++ libc/arch-arm64/denver64/denver64.mk
@@ -1,7 +1,9 @@
 libc_bionic_src_files_arm64 += \
     arch-arm64/denver64/bionic/memcpy.S \
+    arch-arm64/denver64/bionic/memmove.S \
     arch-arm64/denver64/bionic/memset.S \
 
 libc_bionic_src_files_exclude_arm64 += \
     arch-arm64/generic/bionic/memcpy.S \
+    arch-arm64/generic/bionic/memmove.S \
     arch-arm64/generic/bionic/memset.S \
