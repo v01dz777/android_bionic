f8a4a7099843da63a3a784bb51e8ff2961b0213c
Danny Baumann <dannybaumann@web.de>
2016-09-07 16:54:06 +0200

    Inject shim libs as if they were DT_NEEDED.
    The previous separate approach had one flaw: If the shim lib requires
    another lib that's already loaded, find_library_internal() would return
    the previously loaded copy, but the later load action would fail as the
    ELF reader map of the initial loading round was already discarded and
    thus a new ElfReader instance for the soinfo instance was created, which
    didn't know about the previous reading/loading state.
    
    Change-Id: Ib224dbd35d114197097e3dee14a077cc9130fedb

--- linker/linker.cpp
+++ linker/linker.cpp
@@ -1735,6 +1735,7 @@ static void for_each_dt_needed(const soinfo* si, F action) {
       action(fix_dt_needed(si->get_string(d->d_un.d_val), si->get_realpath()));
     }
   }
+  for_each_matching_shim(si->get_realpath(), action);
 }
 
 template<typename F>
@@ -1745,6 +1746,7 @@ static void for_each_dt_needed(const ElfReader& elf_reader, F action) {
       action(fix_dt_needed(elf_reader.get_string(d->d_un.d_val), elf_reader.name()));
     }
   }
+  for_each_matching_shim(elf_reader.name(), action);
 }
 
 static bool load_library(android_namespace_t* ns,
