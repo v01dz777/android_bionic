9844f5474a503d5d7a3b67b301024df03d210b93
Jake Weinstein <xboxlover360@gmail.com>
2016-09-18 17:41:40 -0400

    libc: exclude C memchr on ARM
    * We have an ARM Assembly routine for memchr
    
    Change-Id: I16ab955254a03a407c012337107e8b6bb2b6df32

--- libc/arch-arm/arm.mk
+++ libc/arch-arm/arm.mk
@@ -14,6 +14,7 @@ libc_bionic_src_files_exclude_arm += \
     bionic/__memset_chk.cpp \
 
 libc_openbsd_src_files_exclude_arm += \
+    upstream-openbsd/lib/libc/string/memchr.c \
     upstream-openbsd/lib/libc/string/strcpy.c \
 
 #
